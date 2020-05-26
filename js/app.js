2cfcc2c3c7f75dab47cc5273d252b37633c221f0
esw-l49 <574002934@qq.com>
2020-05-26 22:04:29 +0800

    modified

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // 数组去重
 function deleteRepeat(arr) {
-    return deleteRepeat = [...new setInterval(arr)];
+  return (deleteRepeat = [...new setInterval(arr)]);
 }
 // 查找最大最小
 const arr = [1, 2, 3, 4, 12, 3, 1, 2];
@@ -8,67 +8,67 @@ const max = Math.max(arr);
 const min = Math.min(arr);
 // 返回已 size 为长度的数组分割的原数组
 function chunk(arr, size = 1) {
-    return Array.from(
-        {
-            length: Math.ceil(arr.length / size),
-        },
-        (v, i) => arr.slice(i * size, i * size + size)
-    );
+  return Array.from(
+    {
+      length: Math.ceil(arr.length / size),
+    },
+    (v, i) => arr.slice(i * size, i * size + size)
+  );
 }
 // 检查数组中某元素出现的次数
 function countOccurrences(arr, value) {
-    return arr.reduce((a, v) => (v === value ? a + 1 : a + 0), 0);
+  return arr.reduce((a, v) => (v === value ? a + 1 : a + 0), 0);
 }
 // 扁平化数组
 function flatten(arr, depth = -1) {
-    if (depth === -1) {
-        return [].concat(
-            ...arr.map((v) => (Array.isArray(v) ? this.flatten(v) : v))
-        );
-    }
-    if (depth === 1) {
-        return arr.reduce((a, v) => a.concat(v), []);
-    }
-    return arr.reduce(
-        (a, v) => a.concat(Array.isArray(v) ? this.flatten(v, depth - 1) : v),
-        []
+  if (depth === -1) {
+    return [].concat(
+      ...arr.map((v) => (Array.isArray(v) ? this.flatten(v) : v))
     );
+  }
+  if (depth === 1) {
+    return arr.reduce((a, v) => a.concat(v), []);
+  }
+  return arr.reduce(
+    (a, v) => a.concat(Array.isArray(v) ? this.flatten(v, depth - 1) : v),
+    []
+  );
 }
 // 对比两个数组并且返回其中不同的元素
 function diffrence(arrA, arrB) {
-    return arrA.filter((v) => !arrB.includes(v));
+  return arrA.filter((v) => !arrB.includes(v));
 }
 // 返回两个数组其中相同的元素
 function intersection(arr1, arr2) {
-    return arr2.filter((v) => arr1.includes(v));
+  return arr2.filter((v) => arr1.includes(v));
 }
 // 从右删除 n 个元素
 function dropRight(arr, n = 0) {
-    return n < arr.length ? arr.slice(0, arr.length - n) : [];
+  return n < arr.length ? arr.slice(0, arr.length - n) : [];
 }
 // 截取第一个符合条件的元素及其以后的元素
 function dropElements(arr, fn) {
-    while (arr.length && !fn(arr[0])) arr = arr.slice(1);
-    return arr;
+  while (arr.length && !fn(arr[0])) arr = arr.slice(1);
+  return arr;
 }
 //   返回数组中下标间隔 nth 的元素
 function everyNth(arr, nth) {
-    return arr.filter((v, i) => i % nth === nth - 1);
+  return arr.filter((v, i) => i % nth === nth - 1);
 }
 // 数组随机重排
 function shuffle(arr) {
-    let array = arr;
-    let index = array.length;
+  let array = arr;
+  let index = array.length;
 
-    while (index) {
-        index -= 1;
-        let randomInedx = Math.floor(Math.random() * index);
-        let middleware = array[index];
-        array[index] = array[randomInedx];
-        array[randomInedx] = middleware;
-    }
+  while (index) {
+    index -= 1;
+    let randomInedx = Math.floor(Math.random() * index);
+    let middleware = array[index];
+    array[index] = array[randomInedx];
+    array[randomInedx] = middleware;
+  }
 
-    return array;
+  return array;
 }
 // 判读浏览器是否支持 CSS 属性
 /**
@@ -77,36 +77,36 @@ function shuffle(arr) {
  * @returns {String} - 支持的属性情况
  */
 function validateCssKey(key) {
-    const jsKey = toCamelCase(key); // 有些css属性是连字符号形成
-    if (jsKey in document.documentElement.style) {
-        return key;
+  const jsKey = toCamelCase(key); // 有些css属性是连字符号形成
+  if (jsKey in document.documentElement.style) {
+    return key;
+  }
+  let validKey = "";
+  // 属性名为前缀在js中的形式，属性值是前缀在css中的形式
+  // 经尝试，Webkit 也可是首字母小写 webkit
+  const prefixMap = {
+    Webkit: "-webkit-",
+    Moz: "-moz-",
+    ms: "-ms-",
+    O: "-o-",
+  };
+  for (const jsPrefix in prefixMap) {
+    const styleKey = toCamelCase(`${jsPrefix}-${jsKey}`);
+    if (styleKey in document.documentElement.style) {
+      validKey = prefixMap[jsPrefix] + key;
+      break;
     }
-    let validKey = "";
-    // 属性名为前缀在js中的形式，属性值是前缀在css中的形式
-    // 经尝试，Webkit 也可是首字母小写 webkit
-    const prefixMap = {
-        Webkit: "-webkit-",
-        Moz: "-moz-",
-        ms: "-ms-",
-        O: "-o-",
-    };
-    for (const jsPrefix in prefixMap) {
-        const styleKey = toCamelCase(`${jsPrefix}-${jsKey}`);
-        if (styleKey in document.documentElement.style) {
-            validKey = prefixMap[jsPrefix] + key;
-            break;
-        }
-    }
-    return validKey;
+  }
+  return validKey;
 }
 
 /**
  * 把有连字符号的字符串转化为驼峰命名法的字符串
  */
 function toCamelCase(value) {
-    return value.replace(/-(\w)/g, (matched, letter) => {
-        return letter.toUpperCase();
-    });
+  return value.replace(/-(\w)/g, (matched, letter) => {
+    return letter.toUpperCase();
+  });
 }
 
 /**
@@ -116,18 +116,18 @@ function toCamelCase(value) {
  * @returns {String} - 返回浏览器支持的属性值
  */
 function valiateCssValue(key, value) {
-    const prefix = ["-o-", "-ms-", "-moz-", "-webkit-", ""];
-    const prefixValue = prefix.map((item) => {
-        return item + value;
-    });
-    const element = document.createElement("div");
-    const eleStyle = element.style;
-    // 应用每个前缀的情况，且最后也要应用上没有前缀的情况，看最后浏览器起效的何种情况
-    // 这就是最好在prefix里的最后一个元素是''
-    prefixValue.forEach((item) => {
-        eleStyle[key] = item;
-    });
-    return eleStyle[key];
+  const prefix = ["-o-", "-ms-", "-moz-", "-webkit-", ""];
+  const prefixValue = prefix.map((item) => {
+    return item + value;
+  });
+  const element = document.createElement("div");
+  const eleStyle = element.style;
+  // 应用每个前缀的情况，且最后也要应用上没有前缀的情况，看最后浏览器起效的何种情况
+  // 这就是最好在prefix里的最后一个元素是''
+  prefixValue.forEach((item) => {
+    eleStyle[key] = item;
+  });
+  return eleStyle[key];
 }
 
 /**
@@ -137,93 +137,99 @@ function valiateCssValue(key, value) {
  * @returns {String} - 返回浏览器支持的属性值
  */
 function valiateCssValue(key, value) {
-    var prefix = ["-o-", "-ms-", "-moz-", "-webkit-", ""];
-    var prefixValue = [];
-    for (var i = 0; i < prefix.length; i++) {
-        prefixValue.push(prefix[i] + value);
-    }
-    var element = document.createElement("div");
-    var eleStyle = element.style;
-    for (var j = 0; j < prefixValue.length; j++) {
-        eleStyle[key] = prefixValue[j];
-    }
-    return eleStyle[key];
+  var prefix = ["-o-", "-ms-", "-moz-", "-webkit-", ""];
+  var prefixValue = [];
+  for (var i = 0; i < prefix.length; i++) {
+    prefixValue.push(prefix[i] + value);
+  }
+  var element = document.createElement("div");
+  var eleStyle = element.style;
+  for (var j = 0; j < prefixValue.length; j++) {
+    eleStyle[key] = prefixValue[j];
+  }
+  return eleStyle[key];
 }
 
 function validCss(key, value) {
-    const validCss = validateCssKey(key);
-    if (validCss) {
-        return validCss;
-    }
-    return valiateCssValue(key, value);
+  const validCss = validateCssKey(key);
+  if (validCss) {
+    return validCss;
+  }
+  return valiateCssValue(key, value);
 }
 
 // 返回当前网页地址
 function currentURL() {
-    return window.location.href;
+  return window.location.href;
 }
 // 获取滚动条位置
 function getScrollPosition(el = window) {
-    return {
-        x: el.pageXOffset !== undefined ? el.pageXOffset : el.scrollLeft,
-        y: el.pageYOffset !== undefined ? el.pageYOffset : el.scrollTop,
-    };
+  return {
+    x: el.pageXOffset !== undefined ? el.pageXOffset : el.scrollLeft,
+    y: el.pageYOffset !== undefined ? el.pageYOffset : el.scrollTop,
+  };
 }
 // 获取 url 中的参数
 function getURLParameters(url) {
-    return url
-        .match(/([^?=&]+)(=([^&]+))/g)
-        .reduce(
-            (a, v) => (a[v.slice(0, v.indexOf('='))] = v.slice(v.indexOf('=') + 1), a),
-            {}
-        );
+  return url
+    .match(/([^?=&]+)(=([^&]+))/g)
+    .reduce(
+      (a, v) => (
+        (a[v.slice(0, v.indexOf("="))] = v.slice(v.indexOf("=") + 1)), a
+      ),
+      {}
+    );
 }
 //  表达式(b=a*2,b)返回第二个表达式b的值
 // console.log('ttp://hfuaf.com.cn?name=jing&user=haha'.match(/([^?=&]+)(=([^&]+))/g));
-// console.log(getURLParameters('http://hfuaf.com.cn?name=jing&user=haha'));
+// console.log(getURLParameters("http://hfuaf.com.cn?name=jing&user=haha"));
 
 // 页面跳转，是否记录在 history 中
 function redirect(url, asLink = true) {
-    asLink ? (window.location.href = url) : window.location.replace(url);
+  asLink ? (window.location.href = url) : window.location.replace(url);
 }
 // 检测设备类型
 function detectDeviceType() {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-        navigator.userAgent
-    )
-        ? "Mobile"
-        : "Desktop";
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  )
+    ? "Mobile"
+    : "Desktop";
 }
 // 进入全屏
 function launchFullscreen(element) {
-    if (element.requestFullscreen) {
-        element.requestFullscreen();
-    } else if (element.mozRequestFullScreen) {
-        element.mozRequestFullScreen();
-    } else if (element.msRequestFullscreen) {
-        element.msRequestFullscreen();
-    } else if (element.webkitRequestFullscreen) {
-        element.webkitRequestFullScreen();
-    }
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if (element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullScreen();
+  }
 }
 // 退出全屏
 function exitFullscreen() {
-    if (document.exitFullscreen) {
-        document.exitFullscreen();
-    } else if (document.msExitFullscreen) {
-        document.msExitFullscreen();
-    } else if (document.mozCancelFullScreen) {
-        document.mozCancelFullScreen();
-    } else if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-    }
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
 }
 
 // 全屏事件
 document.addEventListener("fullscreenchange", function (e) {
-    if (document.fullscreenElement) {
-        console.log("进入全屏");
-    } else {
-        console.log("退出全屏");
-    }
+  if (document.fullscreenElement) {
+    console.log("进入全屏");
+  } else {
+    console.log("退出全屏");
+  }
 });
+Router.route("/red", function () {
+  document.querySelector("#article").style.backgroundColor = "blue";
+});
+console.log(location);
